7bb5ebcbc5024814f2ed1a30f8a849270a2d44d0
Cosmin <cuibus.cosmin99@gmail.com>
2024-01-29 09:51:33 +0200

    volunatries by id

---
+++ components/dash/VoluntaryInforCardCompany.tsx
@@ -0,0 +1,96 @@
+import router from "next/router";
+import { Voluntary, Location } from "../../Interfaces/Interface";
+import Icon from "../icon/Icon";
+import Button from "../input/Button";
+import axios from "axios";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useState } from "react";
+import AppliedSuccessfully from "../AppliedSuccesfully";
+
+export default function VoluntaryInforCardCompany(props: Voluntary) {
+  const { user, error, isLoading } = useUser();
+  const [hide, setHide] = useState<boolean>(false);
+
+  function mapsURL(address: Location) {
+    return (
+      "http://maps.google.com/?q=" +
+      address.city +
+      " " +
+      address.street +
+      " " +
+      address.number
+    );
+  }
+  debugger;
+
+  return (
+    <div>
+      {!hide ? (
+        <div className="bg-slate-200 border-slate-400 border-2 rounded-xl flex flex-col flex-grow">
+          <span className="mt-4 text-center text-4xl font-bold">
+            {props.name}
+          </span>
+          <div className="flex flex-col gap-4 ml-8">
+            <div className="flex items-center">
+              <Icon className="las la-calendar-alt text-4xl" />
+              <label>Start Date: </label>
+              <span className="ml-1">{props.startDate}</span>
+            </div>
+            <div className="flex items-center">
+              <Icon className="las la-calendar-alt text-4xl" />
+              <label>End Date: </label>
+              <span className="ml-1">{props.endDate}</span>
+            </div>
+            <div className="flex items-center">
+              <Icon className="las la-clock text-4xl" />
+              <span className="m-1 gap-4">
+                Duration:{" "}
+                {(Date.parse(props.endDate) - Date.parse(props.startDate)) /
+                  (1000 * 3600 * 24)}{" "}
+                days
+              </span>
+            </div>
+            <div className="flex items-center">
+              <Icon className="las la-info-circle text-4xl text-blue-700" />
+              <span>{props.description}</span>
+            </div>
+            <div className="flex items-center">
+              <Icon className="las la-map-pin text-4xl text-red-600" />
+              <a
+                href={mapsURL(props.location)}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>
+                  {props.location.street +
+                    " " +
+                    props.location.number +
+                    ", " +
+                    props.location.city}
+                </span>
+              </a>
+            </div>
+            <div className="flex items-center">
+              <Icon className="las la-award text-4xl text-yellow-600" />
+              <span>{props.reward}</span>
+            </div>
+            <div className="flex items-end">
+              <div className=" m-5">
+                <Button size="base" className="mt-4 ml-auto" color="green">
+                  Edit
+                </Button>
+              </div>
+              <div className=" m-5">
+                <Button size="base" className="mt-4 ml-auto" color="yellow">
+                  See Requests
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <AppliedSuccessfully />
+      )}
+    </div>
+  );
+}
--- pages/home/company-dashboard.tsx
+++ pages/home/company-dashboard.tsx
@@ -1,9 +1,57 @@
 import Navbar from "../../components/Nav/Navbar";
+import { useRouter } from "next/router";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useEffect, useState } from "react";
+import { Voluntary } from "../../Interfaces/Interface";
+import axios from "axios";
+import VoluntaryInfoCard from "../../components/dash/VoluntaryInfoCard";
+import VoluntaryInforCardCompany from "../../components/dash/VoluntaryInforCardCompany";
+import NoVoluntary from "../../components/NoVoluntary";
 
 export default function Dashboard() {
+  const router = useRouter();
+  const { voluntaryId } = router.query;
+  const [voluntarys, setVoluntarys] = useState<Voluntary[]>();
+
+  const { user, error, isLoading } = useUser();
+  console.log(user?.sub);
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`https://localhost:7129/api/Voluntary/company/${user.sub}`, {})
+        .then((response) => {
+          setVoluntarys(response.data);
+          console.log(response.data);
+          console.log(voluntarys);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [user?.sub]);
+
   return (
     <div>
-      <Navbar /> <div>alex</div>
+      <Navbar />
+      <div className="grid grid-cols-1 m-52 gap-20">
+        {voluntarys?.length != 0 ? (
+          voluntarys?.map((x, index) => (
+            <VoluntaryInforCardCompany
+              key={index}
+              location={x.location}
+              name={x.name}
+              company={x.company}
+              description={x.description}
+              endDate={x.endDate}
+              id={x.id}
+              startDate={x.startDate}
+              reward={x.reward}
+            />
+          ))
+        ) : (
+          <NoVoluntary />
+        )}
+      </div>
     </div>
   );
 }
